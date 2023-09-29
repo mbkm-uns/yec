29cd2828f38bac2b7b7bc1485255b208ee6642f3
Irvan Alfian Adi N <92213859+Irvana875Uns@users.noreply.github.com>
2023-09-30 01:18:36 +0700

    login dan register

---
+++ src/app/auth/login/page.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<section class="relative flex flex-wrap lg:h-screen lg:items-center">
+  <div class="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
+    <div class="mx-auto max-w-lg text-center">
+      <h1 class="text-2xl font-bold sm:text-3xl">Selamat Datang Di YEC CO ID</h1>
+
+      <p class="mt-4 text-gray-500">
+        Silahkan masukkan e-mail dan password terlebih dahulu
+      </p>
+    </div>
+
+    <form action="" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
+      <div>
+        <label for="email" class="sr-only">Email</label>
+
+        <div class="relative">
+          <input
+            type="email"
+            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+            placeholder="Enter email"
+          />
+
+        </div>
+      </div>
+
+      <div>
+        <label for="password" class="sr-only">Password</label>
+
+        <div class="relative">
+          <input
+            type="password"
+            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+            placeholder="Enter password"
+          />
+
+          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-4 w-4 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <p class="text-sm text-gray-500">
+          No account?
+          <a class="underline" href="">Sign up</a>
+        </p>
+
+        <button
+          type="submit"
+          class="inline-block rounded-lg bg-orange-500 px-5 py-3 text-sm font-medium text-white"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
+  </div>
+
+  <div class="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
+  <img src="@/assets/images/landingpage/cover3.jpg" class="w-full py-40">
+  </div>
+</section>
+</template>
