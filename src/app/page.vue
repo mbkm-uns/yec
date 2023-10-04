8090b8f0a948f9aa6a263bd85dca1cf74ea50d9d
Irvan Alfian Adi N <92213859+Irvana875Uns@users.noreply.github.com>
2023-10-04 14:20:06 +0700

    UPDATE PRAKERJA DAN FAQ

--- src/app/page.vue
+++ src/app/page.vue
@@ -11,9 +11,6 @@ const { data: classes } = useHttp('users/v1/public/program/study_group')
         <img
           class="carousel-img mx-auto w-full rounded-lg"
           src="@/assets/images/landingpage/tutor_redeem.png"
-          a
-          href
-          link="https://www.youtube.com/watch?v=1rp8royyiQ8"
         />
         <img
           class="carousel-img mx-auto w-full rounded-lg"
