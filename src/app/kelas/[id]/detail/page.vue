4071f83e14502284773a8d0d99e1fe24a36bab26
Ajengwahyu <ajengwahyu@student.uns.ac.id>
2023-10-13 16:34:00 +0700

    update sementara

--- src/app/kelas/[id]/detail/page.vue
+++ src/app/kelas/[id]/detail/page.vue
@@ -28,21 +28,38 @@ const showFullText = ref(false)
             <n-tab-pane name="Dekripsi">
               <div class="space-y-5">
                 <div :class="{ 'h-auto': showFullText }">
-                  <p class="text-sm">
-                    <b>Mengelola Kinerja Karyawan Untuk Menjadi Manajer Sumber Daya Manusia</b>
-                    adalah kelas yang disusun untuk peserta yang ingin belajar mengelola kinerja
-                    karyawan khususnya bagi yang berprofesi sebagai manajer sumber daya manusia.
-                    Pelatihan ini menggunakan metode ajar webinar. Kelas ini merujuk pada SKKNI
-                    Nomor 149 Tahun 2020 Tentang Manajemen Sumber Daya Manusia. <br /><br />
-                    Peserta akan mendapatkan pengetahuan tentang Memahami dasar-dasar manajemen
-                    kinerja karyawan, merancang indikator kinerja karyawan, merancang proses
-                    monitoring kinerja karyawan, merancang proses evaluasi kinerja karyawan,
-                    menyusun perencanaan peningkatan kinerja karyawan <br /><br />
-                    Kompetensi yang disasar dalam program ini adalah peserta mampu memahami
-                    dasar-dasar manajemen kinerja karyawan, merancang indikator kinerja karyawan,
-                    bersikap teliti dan cermat dalam menyusun indikator kinerja karyawan, mampu
-                    merancang proses monitoring kinerja karyawan, merancang proses evaluasi kinerja
-                    karyawan, menyusun perencanaan peningkatan kinerja karyawan ...
+                  <div class="space-y-3">
+                    <p class="text-sm">
+                      <b>Mengelola Kinerja Karyawan Untuk Menjadi Manajer Sumber Daya Manusia</b>
+                      adalah kelas yang disusun untuk peserta yang ingin belajar mengelola kinerja
+                      karyawan khususnya bagi yang berprofesi sebagai manajer sumber daya manusia.
+                      Pelatihan ini menggunakan metode ajar webinar. Kelas ini merujuk pada SKKNI
+                      Nomor 149 Tahun 2020 Tentang Manajemen Sumber Daya Manusia.
+                    </p>
+                    <p class="text-sm">
+                      Peserta akan mendapatkan pengetahuan tentang Memahami dasar-dasar manajemen
+                      kinerja karyawan, merancang indikator kinerja karyawan, merancang proses
+                      monitoring kinerja karyawan, merancang proses evaluasi kinerja karyawan,
+                      menyusun perencanaan peningkatan kinerja karyawan.
+                    </p>
+                    <p class="text-sm">
+                      Kompetensi yang disasar dalam program ini adalah peserta mampu memahami
+                      dasar-dasar manajemen kinerja karyawan, merancang indikator kinerja karyawan,
+                      bersikap teliti dan cermat dalam menyusun indikator kinerja karyawan, mampu
+                      merancang proses monitoring kinerja karyawan, merancang proses evaluasi
+                      kinerja karyawan, menyusun perencanaan peningkatan kinerja karyawan.
+                    </p>
+                    <p class="text-sm">Berikut rincian kompetensinya:</p>
+                    <p class="text-lg font-medium">Tujuan Umum</p>
+                    <p class="text-sm">
+                      Peserta pelatihan mampu merumuskan indikator kinerja karyawan hingga menyusun
+                      perencanaan peningkatan kinerja karyawan untuk menunjang pekerjaannya
+                      sehari-hari sebagai Manajer Sumber Daya Manusia dengan menunjukkan minimal
+                      enam puluh persen penguasaan materi pada saat unjuk keterampilan
+                    </p>
+                    <p>Di akhir pelatihan peserta mampu:</p>
+                    <p>Pengetahuan:</p>
+                    <li>Peserta mampu memahami dasar-dasar manajemen kinerja karyawan</li>
                     <button
                       v-if="!showFullText"
                       @click="showFullText = true"
@@ -50,7 +67,7 @@ const showFullText = ref(false)
                     >
                       Lihat Selengkapnya
                     </button>
-                  </p>
+                  </div>
                 </div>
                 <div>
                   <p class="text-lg font-semibold">Bidang Studi</p>
@@ -97,9 +114,9 @@ const showFullText = ref(false)
               </div>
             </n-tab-pane>
             <n-tab-pane name="Ulasan">
-              <n-icon>
-                <i-mdi:comment-quote-outline class="text-7xl text-g" />
-              </n-icon>
+              <div class="flex justify-center items-center">
+                <i-mdi:comment-quote-outline class="text-7xl text-gray-400 text-center" />
+              </div>
               <p class="text-center text-lg font-semibold">Belum ada ulasan untuk kelas ini</p>
             </n-tab-pane>
           </n-tabs>
