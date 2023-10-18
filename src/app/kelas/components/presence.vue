5aa30205ee402ffe5e773323e24b72903c484851
Afif Nugroho <92508998+Apips1@users.noreply.github.com>
2023-10-18 08:42:02 +0700

    Update lagi

--- src/app/kelas/components/presence.vue
+++ src/app/kelas/components/presence.vue
@@ -1,18 +1,18 @@
-<script setup lang="ts">
-const props = defineProps<{
-
-}>()
-
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="space-y-3">
-        <div>Live Webinar Mengelola Kinerja Karyawan Untuk Menjadi Manajer Sumber Daya Manusia: "Aktivitas 1"</div>
-        <div>silahkan untuk mengisi presensi pertemuan 1 dan pastikan data yang Anda isi sudah sesuai</div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeEKFNQerdnkQJpYwYOaR3Ig0OO7fz-QLUSS6rqm-VQrlX-Jg/viewform"
-            class="w-full">
-
-        </iframe>
+  <div class="space-y-3">
+    <div>
+      Live Webinar Mengelola Kinerja Karyawan Untuk Menjadi Manajer Sumber Daya Manusia: "Aktivitas
+      1"
     </div>
+    <div>
+      silahkan untuk mengisi presensi pertemuan 1 dan pastikan data yang Anda isi sudah sesuai
+    </div>
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSeEKFNQerdnkQJpYwYOaR3Ig0OO7fz-QLUSS6rqm-VQrlX-Jg/viewform"
+      class="w-full"
+    >
+    </iframe>
+  </div>
 </template>
