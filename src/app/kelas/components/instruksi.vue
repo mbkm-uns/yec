a3f500f2fbb44e8e8a4dfa462db961b7b5fa3cf8
danangg67 <Dananghadi67@student.uns.ac.id>
2023-10-22 22:53:58 +0700

    button download

--- src/app/kelas/components/instruksi.vue
+++ src/app/kelas/components/instruksi.vue
@@ -1,16 +1,61 @@
 <script setup lang="ts">
+import { useHttpMutation } from "@/composables/http/http";
+import { useMessage } from "naive-ui";
+
+const props = defineProps<{ programId: string; topicId: string; activityId: string }>();
+const message = useMessage();
+const { mutate: getUnlock, isLoading } = useHttpMutation(
+  `/users/v1/member/activity/unlock`,
+  {
+    method: "POST",
+    httpOptions: {
+      // axios options
+      timeout: 30000,
+    },
+    queryOptions: {
+      // vue-query options
+
+      onError: function (data) {
+        // console.log(data)
+        message.error(data.data.message);
+      },
+    },
+  }
+);
+
+const onDownload = () => {
+  getUnlock({
+    program_id: props.programId,
+    activity_id: props.activityId,
+    topic_id: props.topicId,
+  });
+};
 </script>
 <template>
   <div class="pt-20 p-3 md:px-20 space-y-5">
     <div class="text-2xl font-semibold">Instruksi Tugas Praktik Mandiri</div>
-    <n-card class="drop-shadow-md max-w-lg">
-      <div class="text-lg font-semibold">
-        Silahkan unduh file Intruksi berikut untuk mengerjakan tugas praktik mandiri!
-      </div>
-      <div class="flex justify-center items-center flex-col ">
+    <n-card class="drop-shadow-md" style="width: max-content; height: 500px">
+      <div class="flex flex-col items-center justify-center" style="height: 100%">
+        <div
+          class="text-lg font-semibold"
+          style="text-align: center; font-family: 'Poppins', sans-serif"
+        >
+          Silahkan unduh file Instruksi berikut untuk mengerjakan tugas praktik mandiri!
+        </div>
+
         <n-icon size="60" color="#8492A6"><i-ic:sharp-cloud-download /></n-icon>
-        <n-button type="primary" size="large">Donwload</n-button>
+        <a>
+          <n-button
+            round
+            type="primary"
+            size="large"
+            target="_blank"
+            :loading="isLoading"
+            @click="onDownload"
+            >Download</n-button
+          >
+        </a>
       </div>
     </n-card>
   </div>
-</template> 
+</template>
