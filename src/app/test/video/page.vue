f19da0dfc8b37ee1872ad1d9b4cef4b603739a50
Marsa Amalia Sholikhah <marsaam@student.uns.ac.id>
2023-11-14 22:46:38 +0700

    membuat aktivitas video

---
+++ src/app/test/video/page.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CVideo from './video.vue'
+</script>
+<template>
+  <c-video
+    poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+    :srcs="[
+      {
+        src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
+        type: 'video/mp4'
+      }
+    ]"
+  />
+</template>
